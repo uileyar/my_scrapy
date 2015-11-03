a7ea864372c0ad92f6d7fc37639a7f7d53e1d520
uileyar <85773031@qq.com>
2015-11-04 00:25:47 +0800

    1

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # my_scrapy
+https://github.com/uileyar/my_scrapy.git
--- my_scrapy/items.py
+++ my_scrapy/items.py
@@ -10,5 +10,8 @@ import scrapy
 
 class MyScrapyItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
+    desc = scrapy.Field()
+
     pass
