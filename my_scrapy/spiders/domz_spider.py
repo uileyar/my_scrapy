b5b681d03aa206c58edb39dca3d7b76719b7d81f
uileyar <85773031@qq.com>
2015-11-04 17:54:41 +0800

    step 1

---
+++ my_scrapy/spiders/domz_spider.py
@@ -0,0 +1,16 @@
+__author__ = 'root'
+
+import scrapy
+
+class DomzSpider (scrapy.Spider):
+    name = "domz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        filename = response.url.split("/")[-2]
+        with open(filename, "wb") as f:
+            f.write(response.body)
